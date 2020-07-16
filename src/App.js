2350f228d06a1de9becdb5a182a2a2096216c850
wolf-math <aaron.wolf@gmail.com>
2020-07-16 16:54:15 +0300

    style

--- src/App.js
+++ src/App.js
@@ -35,6 +35,12 @@ function App() {
           <div className="location">New Tokyo City</div>
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
+        <div className="weather-box">
+          <div className="temp">
+            15
+          </div>
+          <div className="weather">sunny</div>
+        </div>
         
       </main>
     </div>
