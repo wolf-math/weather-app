ccfea96146aaf0362a99d348a66c5628d8e3f4be
wolf-math <aaron.wolf@gmail.com>
2020-07-16 14:20:27 +0300

    styling and date

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="App">
       <main>
@@ -18,6 +31,11 @@ function App() {
             placeholder="search..."
             />
         </div>
+        <div className="location-box">
+          <div className="location">New Tokyo City</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        
       </main>
     </div>
   );
